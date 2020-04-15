857b076c356ab02a9769c6c58c1c35ff01ae0c26
zitch731 <61033198+zitch731@users.noreply.github.com>
2020-04-15 13:58:17 -0700

    Update and rename oneSidedAllocation to oneSidedAllocation.c

---
+++ oneSidedAllocation.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// function definition
+static int oneSidedAllocation(void *pvParameters);
+
+// main function
+int main(void)
+{
+
+}
+
+static int oneSidedAllocation(void *pvParameters) {
+
+}
