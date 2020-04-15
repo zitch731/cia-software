ef3e8cd973ad62b054316ab9fd7169d948af1936
zitch731 <61033198+zitch731@users.noreply.github.com>
2020-04-15 13:59:41 -0700

    Update and rename twoSidedAllocation to twoSidedAllocation.c

---
+++ twoSidedAllocation.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// function definition
+static int twoSidedAllocation(void *pvParameters);
+
+// main function (for testing)
+int main(void)
+{
+
+}
+
+static int twoSidedAllocation(void *pvParameters) {
+
+}
+
